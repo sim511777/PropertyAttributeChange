8430fb63d2a88848b4f7e67b9d15b4cdbe4e9a51
sim511777 <sim51177@gmail.com>
2020-07-24 15:31:19 +0900

    초기 커밋.

---
+++ AttributeChange/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace AttributeChange {
+    partial class Form1 {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent() {
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.propertyGrid2 = new System.Windows.Forms.PropertyGrid();
+            this.SuspendLayout();
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Location = new System.Drawing.Point(12, 12);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.PropertySort = System.Windows.Forms.PropertySort.Categorized;
+            this.propertyGrid1.Size = new System.Drawing.Size(297, 353);
+            this.propertyGrid1.TabIndex = 0;
+            // 
+            // propertyGrid2
+            // 
+            this.propertyGrid2.Location = new System.Drawing.Point(325, 12);
+            this.propertyGrid2.Name = "propertyGrid2";
+            this.propertyGrid2.PropertySort = System.Windows.Forms.PropertySort.Categorized;
+            this.propertyGrid2.Size = new System.Drawing.Size(297, 353);
+            this.propertyGrid2.TabIndex = 1;
+            this.propertyGrid2.PropertyValueChanged += new System.Windows.Forms.PropertyValueChangedEventHandler(this.propertyGrid2_PropertyValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 439);
+            this.Controls.Add(this.propertyGrid2);
+            this.Controls.Add(this.propertyGrid1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.PropertyGrid propertyGrid2;
+    }
+}
+
---
+++ AttributeChange/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AttributeChange {
+    public partial class Form1 : Form {
+        private MyClass obj = new MyClass();
+        private AttributeOption option = new AttributeOption();
+        public Form1() {
+            InitializeComponent();
+            propertyGrid1.SelectedObject = obj;
+            propertyGrid2.SelectedObject = option;
+        }
+
+        private void propertyGrid2_PropertyValueChanged(object s, PropertyValueChangedEventArgs e) {
+            SetPropertyAttributeReadOnly(typeof(MyClass), "Item", option.ReadOnly);
+            SetPropertyAttributeDisplayName(typeof(MyClass), "Item", option.DisplayName);
+            propertyGrid1.Refresh();
+        }
+        
+        public static void SetPropertyAttributeReadOnly(Type type, string propertyName, bool value) {
+            SetPropertyAttributeField(type, propertyName, typeof(ReadOnlyAttribute), "isReadOnly", value);
+        }
+
+        public static void SetPropertyAttributeDisplayName(Type type, string propertyName, string value) {
+            SetPropertyAttributeField(type, propertyName, typeof(DisplayNameAttribute), "_displayName", value);
+        }
+
+        public static void SetPropertyAttributeField(Type type, string propertyName, Type attributeType, string attributeFieldName, object attributeFieldValue) {
+            PropertyDescriptor typeProp = TypeDescriptor.GetProperties(type)[propertyName];   // 타입과 이름으로 프로퍼티 구함
+            var attribute = typeProp.Attributes[attributeType];                               // 프로퍼티와 어트리뷰트타입으로 프로퍼티의 어트리뷰트 구함
+            var attrField = attribute.GetType().GetField(attributeFieldName, BindingFlags.NonPublic | BindingFlags.Instance);  // 필드이름으로 필드 구함
+            attrField.SetValue(attribute, attributeFieldValue);     // 필드 값 변경
+        }
+    }
+
+    public class MyClass {
+        [DisplayName("이름")]
+        [ReadOnly(false)]
+        public int Item { get; set; } = 0;
+    }
+
+    public class AttributeOption {
+        public string DisplayName { get; set; } = "이름";
+        public bool ReadOnly { get; set; } = false;
+    }
+}
